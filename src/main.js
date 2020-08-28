f9efd7682aa4e871ebf6b902eb82968cdbecd2e1
ikhsanalatsary <skull.saders18@gmail.com>
2020-08-28 16:26:10 +0700

    apollo graphql integration

--- src/main.js
+++ src/main.js
@@ -1,18 +1,38 @@
 import Vue from "vue";
 import Vuesax from "vuesax";
 import { Plugin } from "vue-fragment";
+import ApolloClient from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import VueApollo from "vue-apollo";
 import "vuesax/dist/vuesax.css"; //Vuesax styles
 import "material-icons/iconfont/material-icons.css";
 import App from "./App.vue";
 import router from "./router";
 
-Vue.use(Vuesax, {
-  // options here
-}).use(Plugin);
+Vue.use(Vuesax).use(Plugin).use(VueApollo);
 
 Vue.config.productionTip = false;
 
+// Create an http link:
+const link = new HttpLink({
+  uri: "https://active-beagle-94.hasura.app/v1beta1/relay",
+  fetch,
+});
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache({
+    addTypename: true,
+  }),
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: client,
+});
+
 new Vue({
+  apolloProvider,
   render: (h) => h(App),
   router,
 }).$mount("#app");

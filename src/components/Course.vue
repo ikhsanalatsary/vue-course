5d4136e55c863760b70243475b59e29b82d162bc
ikhsanalatsary <skull.saders18@gmail.com>
2020-08-28 17:11:42 +0700

    query course event via apollo

--- src/components/Course.vue
+++ src/components/Course.vue
@@ -1,33 +1,34 @@
 <template>
   <vs-row vs-justify="center" vs-w="8">
-    <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
+    <vs-col
+      type="flex"
+      vs-justify="center"
+      vs-align="center"
+      vs-w="6"
+      v-for="data in courseEvents.edges"
+      :key="data.node.id"
+    >
       <vs-card class="cardx" fixedHeight actionable>
         <div slot="header">
-          <h3>Hello world !</h3>
+          <h3>{{ data.node.course.name }}</h3>
         </div>
         <div slot="media">
           <img src="https://lusaxweb.github.io/vuesax/card.png" />
         </div>
         <div>
-          <span
-            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.</span
-          >
-          <span
-            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.</span
-          >
+          <p>{{ data.node.course.description }}</p>
         </div>
         <div slot="footer">
           <vs-row vs-justify="flex-end">
             <vs-button
               color="primary"
               icon="visibility"
-              @click="handleClick('0')"
+              @click="
+                handleClick(
+                  { id: data.node.id, publicId: data.node.publicId },
+                  event
+                )
+              "
               >View</vs-button
             >
           </vs-row>
@@ -52,12 +53,7 @@
         </div>
         <div slot="footer">
           <vs-row vs-justify="flex-end">
-            <vs-button
-              color="primary"
-              icon="visibility"
-              @click="handleClick('1')"
-              >View</vs-button
-            >
+            <vs-button color="primary" icon="visibility">View</vs-button>
           </vs-row>
         </div>
       </vs-card>
@@ -66,16 +62,59 @@
 </template>
 
 <script>
+import gql from "graphql-tag";
+
+let COURSE_EVENTS_CONNECTION = gql`
+  query Course {
+    courseEvents: courseEvents_connection(first: 10) {
+      pageInfo {
+        endCursor
+        hasNextPage
+        hasPreviousPage
+        startCursor
+      }
+      edges {
+        cursor
+        node {
+          id
+          publicId
+          course {
+            name
+            description
+          }
+        }
+      }
+    }
+  }
+`;
 export default {
   name: "Course",
+  data() {
+    console.log(this);
+    return {
+      courseEvents: {
+        edges: [],
+        pageInfo: {},
+      },
+    };
+  },
   methods: {
-    handleClick(index) {
-      console.log("jal", index);
+    handleClick({ id, publicId }) {
+      console.log("jal", id, publicId);
       console.log("this.props", this.$router);
       this.$router.push({
         name: "course-detail",
-        params: { publicId: index, id: 123 },
+        params: { publicId, id },
       });
+    },
+  },
+  apollo: {
+    courseEvents: {
+      query: COURSE_EVENTS_CONNECTION,
+      variables() {
+        // Use vue reactive properties here
+        return {};
+      },
     },
   },
 };

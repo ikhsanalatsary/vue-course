20b03b95d416b4590def9590ab22113f8796d2aa
ikhsanalatsary <skull.saders18@gmail.com>
2020-08-30 02:12:43 +0700

    mutation student with optimistic update

--- src/components/Student.vue
+++ src/components/Student.vue
@@ -1,38 +1,65 @@
 <template lang="html">
   <vs-row vs-justify="center" vs-w="8">
     <vs-col type="flex" vs-justify="center">
-    <form class="centerx labelx">
-      <vs-input label="Name" placeholder="Student full name" v-model="name"/>
-      <vs-input label="DOB" placeholder="Date of birth" v-model="birthday"/>
-      <vs-button color="primary" type="border">Save</vs-button>
-    </form>
+      <form class="centerx labelx">
+        <vs-input
+          :danger="validationField.name"
+          danger-text="This field is required!"
+          label="Name"
+          placeholder="Student full name"
+          v-model="name"
+        />
+        <vs-input
+          label="DOB"
+          :danger="validationField.dob"
+          danger-text="This field is required!"
+          placeholder="Date of birth"
+          v-model="birthday"
+        />
+        <vs-button color="primary" type="border" @click="addStudent"
+          >Save</vs-button
+        >
+      </form>
     </vs-col>
     <vs-col>
       <vs-table stripe :data="students.edges">
         <template v-slot:header>
-          <h3>
-            Student
-          </h3>
+          <h3>Student</h3>
         </template>
-      <template v-slot:thead>
-        <vs-th>Name</vs-th>
-        <vs-th>Birthday</vs-th>
-        <vs-th>Nro</vs-th>
-        <vs-progress v-if="$apollo.queries.students.loading" indeterminate color="primary">primary</vs-progress>
-      </template>
+        <template v-slot:thead>
+          <vs-th>Name</vs-th>
+          <vs-th>Birthday</vs-th>
+          <vs-th>Nro</vs-th>
+          <vs-progress
+            v-if="$apollo.queries.students.loading"
+            indeterminate
+            color="primary"
+            >primary</vs-progress
+          >
+        </template>
 
-        <template v-slot:default="{data}">
+        <template v-slot:default="{ data }">
           <vs-tr :key="item.node.id" v-for="item in data">
-            <vs-td :data="item.node.name">{{item.node.name}}</vs-td>
+            <vs-td :data="item.node.name">{{ item.node.name }}</vs-td>
 
-            <vs-td :data="item.node.birthday">{{item.node.birthday || '-'}}</vs-td>
+            <vs-td :data="item.node.birthday">{{
+              item.node.birthday || "-"
+            }}</vs-td>
 
-            <vs-td :data="item.node.publicId">{{item.node.publicId}}</vs-td>
+            <vs-td :data="item.node.publicId">{{ item.node.publicId }}</vs-td>
           </vs-tr>
         </template>
       </vs-table>
-      <vs-row vs-type="flex" vs-justify="center" vs-align="center" vs-w="12">  
-        <vs-button ref="loadableButton" class="vs-con-loading__container" type="border" v-if="students.pageInfo.hasNextPage" :disabled="$apollo.queries.students.loading" v-on:click="loadMore">Load more</vs-button>
+      <vs-row vs-type="flex" vs-justify="center" vs-align="center" vs-w="12">
+        <vs-button
+          ref="loadableButton"
+          class="vs-con-loading__container"
+          type="border"
+          v-if="students.pageInfo.hasNextPage"
+          :disabled="$apollo.queries.students.loading"
+          v-on:click="loadMore"
+          >Load more</vs-button
+        >
       </vs-row>
     </vs-col>
   </vs-row>
@@ -40,6 +67,7 @@
 
 <script>
 import gql from "graphql-tag";
+import faker from "faker";
 
 let STUDENTS_CONNECTION = gql`
   query Students($first: Int = 10, $after: String) {
@@ -51,7 +79,6 @@ let STUDENTS_CONNECTION = gql`
         startCursor
       }
       edges {
-        cursor
         node {
           birthday
           id
@@ -70,17 +97,21 @@ export default {
         // Use vue reactive properties here
         return {
           first: this.first,
-          after: this.after
+          after: this.after,
         };
-      }
-    }
+      },
+    },
   },
   data: () => ({
-    name: '',
-    birthday: '',
+    validationField: {
+      name: false,
+      dob: false,
+    },
+    name: "",
+    birthday: "",
     students: {
       edges: [],
-      pageInfo: {}
+      pageInfo: {},
     },
     first: 10,
     after: null,
@@ -167,12 +198,12 @@ export default {
         $vs,
         backgroundLoading,
         colorLoading,
-        $refs
+        $refs,
       } = this;
       let {
         vm: {
-          students: { pageInfo }
-        }
+          students: { pageInfo },
+        },
       } = $apollo;
       console.log($apollo);
       console.log($vs);
@@ -180,12 +211,12 @@ export default {
         background: backgroundLoading,
         color: colorLoading,
         container: $refs.loadableButton.$el,
-        scale: 0.45
+        scale: 0.45,
       });
       $apollo.queries.students.fetchMore({
         variables: {
           first,
-          after: pageInfo.endCursor
+          after: pageInfo.endCursor,
         },
         updateQuery: (previousResult, { fetchMoreResult }) => {
           console.log("previousResult", previousResult);
@@ -200,13 +231,111 @@ export default {
               __typename: previousResult.students.__typename,
               // Merging the tag list
               edges: [...previousResult.students.edges, ...newStudents],
-              pageInfo
-            }
+              pageInfo,
+            },
           };
-        }
+        },
       });
-    }
-  }
+    },
+    addStudent() {
+      if (!this.name) {
+        this.validationField.name = true;
+        this.validationField.dob = true;
+        return;
+      }
+      // eslint-disable-next-line no-unused-vars
+      let name = this.name;
+      let birthday = this.birthday;
+      this.name = this.birthday = "";
+      this.$apollo
+        .mutate({
+          // Query
+          mutation: gql`
+            mutation StudentMutation($name: String!, $birthday: date) {
+              insertStudent(objects: { name: $name, birthday: $birthday }) {
+                affected_rows
+                returning {
+                  birthday
+                  id
+                  name
+                  publicId
+                }
+              }
+            }
+          `,
+          // Parameters
+          variables: {
+            name,
+            birthday,
+          },
+          // Update the cache with the result
+          // The query will be updated with the optimistic response
+          // and then with the real result of the mutation
+          update: (
+            store,
+            {
+              data: {
+                insertStudent: { returning },
+              },
+            }
+          ) => {
+            console.log("addStudent -> returning", returning);
+            console.log("addStudent -> store", store);
+            // Read the data from our cache for this query.
+            const data = store.readQuery({
+              query: STUDENTS_CONNECTION,
+              variables: {
+                first: this.first,
+                after: this.after,
+              },
+            });
+            console.log("addStudent -> data", data);
+            // Add our tag from the mutation to the end
+            let newData = Array.isArray(returning) ? returning[0] : returning;
+            console.log("addStudent -> newData", newData);
+            let mergedData = {
+              node: {
+                ...newData,
+                __typename: "students",
+              },
+              __typename: "studentsEdge",
+            };
+            console.log("addStudent -> mergedData", mergedData);
+            data.students.edges.push(mergedData);
+            // Write our data back to the cache.
+            store.writeQuery({ query: STUDENTS_CONNECTION, data });
+          },
+          // Optimistic UI
+          // Will be treated as a 'fake' result as soon as the request is made
+          // so that the UI can react quickly and the user be happy
+          optimisticResponse: {
+            __typename: "mutation_root",
+            insertStudent: {
+              affected_rows: 1,
+              __typename: "students_mutation_response",
+              returning: {
+                id: -1,
+                name,
+                birthday,
+                publicId: faker.random.uuid(),
+                __typename: "students",
+              },
+            },
+          },
+        })
+        .then((data) => {
+          // Result
+          console.log(data);
+        })
+        .catch((error) => {
+          // Error
+          console.error(error);
+          // We restore the initial user input
+          this.name = name;
+          this.birthday = birthday;
+        });
+    },
+  },
 };
 </script>
 
@@ -214,5 +343,5 @@ export default {
 .labelx .vs-input {
   margin: 10px;
   display: inline-block;
-} 
+}
 </style>

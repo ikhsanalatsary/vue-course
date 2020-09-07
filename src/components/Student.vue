4c1123dd58af6ef9b91efcbd188c23ae7ea26aac
ikhsanalatsary <skull.saders18@gmail.com>
2020-09-07 18:29:07 +0700

    remove unused

--- src/components/Student.vue
+++ src/components/Student.vue
@@ -67,7 +67,6 @@
 
 <script>
 import gql from "graphql-tag";
-import faker from "faker";
 
 let STUDENTS_CONNECTION = gql`
   query Students($first: Int = 10, $after: String) {
@@ -121,78 +120,6 @@ export default {
     after: null,
     backgroundLoading: "primary",
     colorLoading: "#fff",
-    users: [
-      {
-        id: 1,
-        name: "Leanne Graham",
-        username: "Bret",
-        email: "Sincere@april.biz",
-        website: "hildegard.org",
-      },
-      {
-        id: 2,
-        name: "Ervin Howell",
-        username: "Antonette",
-        email: "Shanna@melissa.tv",
-        website: "anastasia.net",
-      },
-      {
-        id: 3,
-        name: "Clementine Bauch",
-        username: "Samantha",
-        email: "Nathan@yesenia.net",
-        website: "ramiro.info",
-      },
-      {
-        id: 4,
-        name: "Patricia Lebsack",
-        username: "Karianne",
-        email: "Julianne.OConner@kory.org",
-        website: "kale.biz",
-      },
-      {
-        id: 5,
-        name: "Chelsey Dietrich",
-        username: "Kamren",
-        email: "Lucio_Hettinger@annie.ca",
-        website: "demarco.info",
-      },
-      {
-        id: 6,
-        name: "Mrs. Dennis Schulist",
-        username: "Leopoldo_Corkery",
-        email: "Karley_Dach@jasper.info",
-        website: "ola.org",
-      },
-      {
-        id: 7,
-        name: "Kurtis Weissnat",
-        username: "Elwyn.Skiles",
-        email: "Telly.Hoeger@billy.biz",
-        website: "elvis.io",
-      },
-      {
-        id: 8,
-        name: "Nicholas Runolfsdottir V",
-        username: "Maxime_Nienow",
-        email: "Sherwood@rosamond.me",
-        website: "jacynthe.com",
-      },
-      {
-        id: 9,
-        name: "Glenna Reichert",
-        username: "Delphine",
-        email: "Chaim_McDermott@dana.io",
-        website: "conrad.com",
-      },
-      {
-        id: 10,
-        name: "Clementina DuBuque",
-        username: "Moriah.Stanton",
-        email: "Rey.Padberg@karina.biz",
-        website: "ambrose.net",
-      },
-    ],
   }),
   methods: {
     loadMore() {
@@ -321,7 +248,7 @@ export default {
                 id: -1,
                 name,
                 birthday,
-                publicId: faker.random.uuid(),
+                publicId: '78770fd8-4de0-4232-9af8-dcbe4d4476e3', // fake
                 __typename: "students",
               },
             },
--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "./components/Home.vue";
-import FormGenData from "./components/FormGenData.vue";
 import Student from "./components/Student.vue";
 import Course from "./components/Course.vue";
 import CourseDetail from "./components/CourseDetail.vue";
@@ -10,7 +9,6 @@ Vue.use(VueRouter);
 
 const routes = [
   { path: "/", component: Home },
-  { path: "/generate", component: FormGenData },
   { path: "/students", component: Student },
   { path: "/course", component: Course },
   {

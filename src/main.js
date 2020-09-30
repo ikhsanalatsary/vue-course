c3b4de0a57c6d7da920782a87855c63e33f33299
Abdul Fattah Ikhsan <skull.saders18@gmail.com>
2020-09-30 14:59:36 +0700

    Graphql (#1)
    * remove headers
    
    * change endpoint
    
    * replace moment with luxon
    
    * feat prerender
    
    * re-arrange

--- src/main.js
+++ src/main.js
@@ -9,21 +9,18 @@ import "vuesax/dist/vuesax.css"; //Vuesax styles
 import "material-icons/iconfont/material-icons.css";
 import App from "./App.vue";
 import router from "./router";
-const moment = require("moment");
-require("moment/locale/id");
-// console.log("sa", moment.locale());
+import { Settings } from "luxon";
+Settings.defaultLocale = "id";
 
 Vue.use(Vuesax)
   .use(Plugin)
-  .use(VueApollo)
-  .use(require("vue-moment"), { moment });
+  .use(VueApollo);
 
 Vue.config.productionTip = false;
 
 // Create an http link:
 const link = new HttpLink({
-  uri: "https://exploregraphql.ikhsan.dev/v1beta1/relay",
-  headers: { "x-hasura-admin-secret": "admin_graphql_ganteng" },
+  uri: "https://graphqlworkshop.ikhsan.dev/v1beta1/relay",
   fetch,
 });
 
